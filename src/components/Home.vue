72cf87faec543c401795fca148d1361a49acf706
wisarut <yeast72@gmail.com>
2017-11-14 14:35:22 +0700

    add footer

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -53,6 +53,7 @@
 
 <script>
 import axios from 'axios'
+
 export default {
   name: 'Hi',
   data () {
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Footer from '@/components/Footer'
 
 Vue.use(Router)
 
@@ -10,7 +11,10 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      components: {
+        default: Home,
+        footer: Footer
+      }
     }
   ]
 })
