861a46eda780cb92fd17f79bcd865f693638b478
Piromsurang Rungserichai <gift.rungserichai@gmail.com>
2017-11-14 12:42:02 +0700

    add map

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,19 @@
     <h1>{{ msg }}</h1>
     <h2 id="ip_header">This is your ip {{info.ip}} </h2>
     <section class="centered-container side-padding">
+      <gmap-map
+      :center="center"
+      :zoom="7"
+      style="width: 500px; height: 300px">
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center=m.position"
+      ></gmap-marker>
+    </gmap-map>
       <table class="table table-striped" align="center">
         <tbody>
           <tr>
@@ -46,6 +59,13 @@
 
 <script>
 import axios from 'axios'
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCNQ60BFvx4nY-EpQVOJ8w3q3v42XsKQe8'
+  }
+})
 export default {
   name: 'Hi',
   data () {
@@ -62,7 +82,9 @@ export default {
         lon: '',
         timezone: '',
         zip: ''
-      }
+      },
+      center: {lat: '', lng: ''},
+      markers: [{position: {lat: '', lng: ''}}, {position: {lat: 11.0, lng: 11.0}}]
     }
   },
   methods: {
@@ -89,6 +111,12 @@ export default {
         this.info.lat = response.data.lat
         this.info.lon = response.data.lon
         this.info.zip = response.data.zip
+        this.center.lat = parseFloat(response.data.lat)
+        this.center.lng = parseFloat(response.data.lon)
+        this.markers[0].lat = parseFloat(response.data.lat)
+        this.markers[0].lng = parseFloat(response.data.lon)
+        this.markers[1].lat = parseFloat(response.data.lat) + 1
+        this.markers[1].lng = parseFloat(response.data.lon) + 1
         this.getInfoIP()
       })
       .catch(e => {
@@ -100,37 +128,4 @@ export default {
     this.getIP()
   }
 }
-// getInfoIP() {
-//     console.log('ip')
-//     axios.get('http://extreme-ip-lookup.com/json/158.108.216.5')
-//       .then(response => {
-//         console.log(response)
-//       })
-//       .catch(e => {
-//         console.log(e)
-//       })
-//   },
-//   getNewIP(ip) {
-//     console.log(ip + '!')
-//     var api = 'http://ip-api.com/json/' + this.ip
-//     axios.get(api)
-//       .then(response => {
-//         console.log(response)
-//         this.info.city = response.data.city
-//         this.info.network = response.data.as
-//         this.info.country = response.data.country
-//         this.info.countryCode = response.data.countryCode
-//         this.info.ip = response.data.query
-//         this.info.timezone = response.data.timezone
-//         this.info.lat = response.data.lat
-//         this.info.lon = response.data.lon
-//         this.info.zip = response.data.zip
-//         this.getInfoIP()
-//       })
-//       .catch(e => {
-//         console.log(e)
-//       })
-//   }
-// })
-// submit.getNewIP()
 </script>
