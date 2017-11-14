e864dca0f2cedf508ef7ecd5a0e349754e8a11f9
crsherbet <warisaracr.y@gmail.com>
2017-11-14 10:30:28 +0700

    can use 2 apis

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,6 +10,14 @@
             <td>{{ info.network }}</td>
           </tr>
           <tr>
+            <td>IP Name</td>
+            <td> {{ info.ipName }} </td>
+          </tr>
+          <tr>
+            <td>IP Type</td>
+            <td> {{ info.ipType}} </td>
+          </tr>
+          <tr>
             <td>City</td>
             <td>
               <a class="flag flag-th"></a>
@@ -21,6 +29,10 @@
             <td> {{info.country}} / {{ info.countryCode }} </td>
           </tr>
           <tr>
+            <td>Continent</td>
+            <td>{{info.continent}} </td>
+          </tr>
+          <tr>
             <td>Latitude/Longitude</td>
             <td> {{info.lat}} / {{info.lon}} </td>
           </tr>
@@ -30,15 +42,10 @@
           </tr>
         </tbody>
       </table>
-      <input id="textarea-ip" v-model="ip" placeholder="New ip address." class="header">
-      <input id="submit-btn" type="submit" class="btn btn_size_big btn_color_green btn_groupped_right" v-on:click="getNewIP(this.textarea-ip.value)">
-      <p>
-        "The average network speed for
-        <b>...</b>
-         in
-        <b>...</b>
-         is shown above. See how your own network speed compares at
-        <a href="http://speedsmart.net/?ipinfo">speedsmart.net</a> "
+      <input id="textarea-ip" v-model="newIP" placeholder="New ip address or website" class="header">
+      <input id="submit-btn" type="submit" class="btn btn_color_green btn_groupped_right" v-on:click="getIP(newIP)">
+        <p/>See how your own network speed compares at
+        <a href="http://speedsmart.net/?ipinfo">speedsmart.net</a> 
       </p>
     </section>
   </div>
@@ -61,35 +68,40 @@ export default {
         lat: '',
         lon: '',
         timezone: '',
-        zip: ''
-      }
+        zip: '',
+        continent: '',
+        ipType: ''
+      },
+      newIP: ''
     }
   },
   methods: {
-    getInfoIP: function () {
-      console.log('ip')
-      axios.get('http://extreme-ip-lookup.com/json/158.108.216.5')
+    getInfoIP: function (ip) {
+      axios.get('http://extreme-ip-lookup.com/json/' + ip)
       .then(response => {
         console.log(response)
+        this.info.ipName = response.data.ipName
+        this.info.ipType = response.data.ipType
+        this.info.timezone = response.data.timezone
+        this.info.lat = response.data.lat
+        this.info.lon = response.data.lon
+        this.info.country = response.data.country
+        this.info.continent = response.data.continent
+        this.info.countryCode = response.data.countryCode
       })
       .catch(e => {
         console.log(e)
       })
     },
-    getIP: function () {
-      axios.get('http://ip-api.com/json')
+    getIP: function (ip) {
+      axios.get('http://ip-api.com/json/' + ip)
       .then(response => {
         console.log(response)
-        this.info.city = response.data.city
         this.info.network = response.data.as
-        this.info.country = response.data.country
-        this.info.countryCode = response.data.countryCode
         this.info.ip = response.data.query
-        this.info.timezone = response.data.timezone
-        this.info.lat = response.data.lat
-        this.info.lon = response.data.lon
         this.info.zip = response.data.zip
-        this.getInfoIP()
+        this.info.city = response.data.city
+        this.getInfoIP(this.info.ip)
       })
       .catch(e => {
         console.log(e)
@@ -97,40 +109,7 @@ export default {
     }
   },
   created () {
-    this.getIP()
+    this.getIP('')
   }
 }
-// getInfoIP() {
-//     console.log('ip')
-//     axios.get('http://extreme-ip-lookup.com/json/158.108.216.5')
-//       .then(response => {
-//         console.log(response)
-//       })
-//       .catch(e => {
-//         console.log(e)
-//       })
-//   },
-//   getNewIP(ip) {
-//     console.log(ip + '!')
-//     var api = 'http://ip-api.com/json/' + this.ip
-//     axios.get(api)
-//       .then(response => {
-//         console.log(response)
-//         this.info.city = response.data.city
-//         this.info.network = response.data.as
-//         this.info.country = response.data.country
-//         this.info.countryCode = response.data.countryCode
-//         this.info.ip = response.data.query
-//         this.info.timezone = response.data.timezone
-//         this.info.lat = response.data.lat
-//         this.info.lon = response.data.lon
-//         this.info.zip = response.data.zip
-//         this.getInfoIP()
-//       })
-//       .catch(e => {
-//         console.log(e)
-//       })
-//   }
-// })
-// submit.getNewIP()
 </script>
